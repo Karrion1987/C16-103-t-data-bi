ec2747cd2b44fe3863192400d9d33fd6900419a0
Victor Raul <v1c70rotakmer@gmail.com>
2024-03-04 15:00:50 -0500

    fix(app): fixing deploy

--- app/pages/04_🕵️‍♂️_Conclusiones.py
+++ app/pages/04_🕵️‍♂️_Conclusiones.py
@@ -3,10 +3,8 @@ import streamlit as st
 st.title("Conclusiones")
 
 st.markdown("""
-- El modelo de predicción de transacciones fraudulentas tiene una precisión del 95%.
-- El 40% de las transacciones fraudulentas son cash-in 
-- El 80% de las transacciones fraudulentas se realizan en horario nocturno
-- La mayoría de las transacciones fraudulentas se realizan en la última semana del mes
-- El 60% de las transacciones fraudulentas son de montos menores a 10000
-- Existe una correlación del 70% entre el monto de la transacción y el tipo de transacción
+## El modelo de Machine Learning llamado Gradient Boosting tiene una precisión del 98%.
+## Hay que tener en cuenta que el modelo no es perfecto y puede haber falsos positivos.
+## Las variables más importantes para la predicción son el monto de la transacción, el saldo anterior del destino y el tipo de transacción.
+## Hay 
 """)
